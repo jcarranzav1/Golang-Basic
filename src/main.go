acde83f6ea036129a55a3fddb2aab5c849407ac4
Juan Carranza <juan.carranza@PER644FNJ37J.local>
2022-08-29 13:53:47 -0500

    Hola Mundo

---
+++ src/main.go
@@ -0,0 +1,11 @@
+package main // un package es el nombre de la carpeta en donde esta guardado. En este caso como es el archivo principal, será main
+
+import "fmt"
+
+/*
+Declaremos la funcion main, la funcion principal que se ejecutará en el codigo
+*/
+
+func main() {
+	fmt.Println("Hello World")
+}
