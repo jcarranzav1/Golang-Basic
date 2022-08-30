415b19c93a9d9c2c17348964d698db62b715199f
Juan Diego <juan.carranza@PER644FNJ37J.local>
2022-08-30 16:36:38 -0500

    defer continue and break

--- src/main.go
+++ src/main.go
@@ -4,26 +4,34 @@ import (
 	"fmt"
 )
 
-func main() {
-	switch modulo := 5 % 2; modulo {
-	case 0:
-		fmt.Println("Es par")
-	default:
-		fmt.Println("Es impar")
-	}
+// Una instrucción defer añade la invocación de la función después de la palabra clave defer en una pila. Todas las invocaciones de la pila en cuestión se invocan cuando la funcion donde se añadieron termina. Debido a que las invocaciones se disponen en una pila, se llaman en el orden “último en entrar” y “primero en salir” (LIFO).
 
-	// Sin condicion
-	mes := 6
-	switch {
-	case mes <= 3:
-		fmt.Println("Primer Trimestre")
-	case mes > 3 && mes <= 6:
-		fmt.Println("Segundo Trimestre")
-	case mes > 6 && mes <= 9:
-		fmt.Println("Tercer Trimestre")
-	case mes > 9 && mes <= 12:
-		fmt.Println("Cuarto Trimestre")
-	default:
-		fmt.Println("Este no es un mes valido")
+func main() {
+	defer fmt.Println("Hola") // se invocara al final de la funcion main
+	defer fmt.Println("entre segundo")
+	fmt.Println("Mundo")
+
+	//otro ejemplo
+	fmt.Println("Inicio")
+	for i := 0; i < 5; i++ {
+		defer fmt.Println(i)
+	}
+	fmt.Println("Fin")
+
+	// Continue y break
+	for i := 0; i < 10; i++ {
+
+		// Continue
+		if i == 2 {
+			continue
+		}
+
+		// Break
+		if i == 8 {
+			fmt.Println("Break")
+			break
+		}
+
+		fmt.Println(i)
 	}
 }
