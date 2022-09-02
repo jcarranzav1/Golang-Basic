eb0b9728f502db386104401d9db72676487f0308
Juan Diego <juan.carranza@PER644FNJ37J.local>
2022-09-01 21:56:51 -0500

    structs

--- src/main.go
+++ src/main.go
@@ -2,62 +2,24 @@ package main
 
 import "fmt"
 
+// structs es un tipo definido por el usuario que permite agrupar/combinar elementos de tipos posiblemente diferentes en un único tipo. Es muy similar al concepto de clases en POO.
+// struct es una colección de campos.
+
+type player struct {
+	x, y    int // podemos hacerlo mas compacto combinando los campos del mismo tipo
+	name    string
+	species string
+	alive   bool
+}
+
 func main() {
-	// Un map asigna claves a valores, parecido a diccionario en python o un hash map
-	//MAPS
+	player1 := player{10, 10, "Juan", "human", true}
+	fmt.Println("Player 1:", player1)
 
-	//Declaration
-	var map_1 = map[string]int32{
-		"Car":      50000,
-		"House":    20000,
-		"Computer": 1000,
-	}
+	fmt.Println("\n***************** other way *************************")
+	var player2 player
 
-	fmt.Println(map_1)
-
-	//Another type of declaration
-	map_2 := make(map[int]int)
-	map_2[10] = 1
-	map_2[12] = 4
-	map_2[8] = 5
-	map_2[11] = 2
-
-	//Displaying a map
-	for i, v := range map_2 {
-		fmt.Println(i, "=", v) //There is no order at the moment of displaying a map
-	}
-
-	//Knowing if a value exist
-	//If we display map_1["Car"] it will show the value
-	value := map_1["Car"]
-	fmt.Println("Car", value)
-
-	//But what if it doesn't exist
-	value = map_1["Vehicle"]
-	fmt.Println("Vehicle", value) //It will show zero
-
-	//If we want to know if it exists, it must be done with the second returing value
-	value, ok := map_1["Vehicle"]
-	//it will return a bool depending on if exists or not
-	fmt.Println("Vehicle exist:", ok, "  value:", value)
-
-	fmt.Println("\n******** Add new key/value pair and update ************")
-
-	map_1["PS5"] = 9000
-	map_1["House"] = 500000
-
-	fmt.Println(map_1)
-
-	fmt.Println("\n******** Delete  key/value pair  ***********")
-	delete(map_1, "PS5")
-	fmt.Println(map_1)
-
-	fmt.Println("\n******** Modficacion del map  ***********")
-	//  Como sabemos los mapas son de tipo referencial. Entonces, cuando asignamos un mapa existente a una nueva variable, ambos mapas aún se refieren a la misma estructura de datos subyacente. Entonces, cuando actualicemos un mapa, se reflejará en otro mapa.
-
-	new_map := map_1
-
-	new_map["Car"] = 1200
-	fmt.Println("map_1:", map_1, " new_map:", new_map)
+	player2.name = "Diego"
+	fmt.Println("Player 2:", player2)
 
 }
