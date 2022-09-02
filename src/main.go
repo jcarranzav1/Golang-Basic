8bfa0b10662c8f9203e7eecd4b4c43ca1879e2e2
Juan Diego <juan.carranza@PER644FNJ37J.local>
2022-09-02 16:28:27 -0500

    structs with pointer

--- src/main.go
+++ src/main.go
@@ -1,32 +1,62 @@
 package main
 
 import (
+	pkPC "JD/Golang/Curso_Basico/src/pcpackage"
 	"fmt"
-
-	pk "JD/Golang/Curso_Basico/src/mypackage"
 )
 
-// structs es un tipo definido por el usuario que permite agrupar/combinar elementos de tipos posiblemente diferentes en un único tipo. Es muy similar al concepto de clases en POO.
-// struct es una colección de campos.
+type pc struct {
+	ram   int
+	disk  int
+	brand string
+}
 
-type Player struct {
-	x, y    int // podemos hacerlo mas compacto combinando los campos del mismo tipo
-	name    string
-	species string
-	alive   bool
+func (myPC pc) ping() { //creamos el metodo ping para el struct pc
+	fmt.Println(myPC.brand, "Pong")
+
+}
+
+func (myPC *pc) duplicateRAM() { //hacer esto me recuerda mucho cuando crear un metodo donde modifique el valor de un propertu usando this
+	myPC.ram = myPC.ram * 2
 }
 
 func main() {
-	var player1 pk.PlayerPublic
-	// var player2 pk.playerPrivate
-	player1.X = 20
-	player1.Name = "juan"
 
-	// player1.money = 100
+	// con & accedemos a la posicion de memoria de una variable, mientra que con * accedemos al valor de la posicion de memoria.
+	// Por eso con & se usa para crear punteros de una variable, y con * es para entregar el valor que tiene ese puntero.
 
-	fmt.Println(player1)
+	a := 50
+	b := &a
 
-	pk.PrintMessage("Hola Platzi")
+	fmt.Println(b)
+	fmt.Println(*b)
 
-	// pk.printMessage1("Hola")
+	*b = 100
+	fmt.Println(a)
+
+	fmt.Println("\n**************** pointer with structs ***********************")
+
+	pc1 := pc{ram: 16, disk: 1000, brand: "Asus"}
+
+	pc1.ping()
+
+	fmt.Println(pc1)
+	pc1.duplicateRAM()
+
+	fmt.Println(pc1)
+	pc1.duplicateRAM()
+
+	fmt.Println(pc1)
+
+	fmt.Println("\n**************** EXERCISE ***********************")
+
+	var pc2 pkPC.PC
+
+	pc2.SetSpecs(64, 1000, "Asus")
+	pc2.PrintSpecs()
+
+	pc2.DuplicateRAM()
+
+	fmt.Println("RAM: ", pc2.GetRam())
+
 }
