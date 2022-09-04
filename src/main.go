745d4744d87c5e063307ca16edfe221a484ee713
Juan Diego <juan.carranza@PER644FNJ37J.local>
2022-09-03 21:23:58 -0500

    go rutines

--- src/main.go
+++ src/main.go
@@ -2,90 +2,93 @@ package main
 
 import (
 	"fmt"
-	"math"
-	"reflect"
+	"sync"
 )
 
-// Si los structs que tenemos en el código tienen métodos que hacen algo en común (Cálculos, obtener data, etc), es posible ejecutar éstos métodos usando una interfaz, de esta forma evitamos hacer código por cada struct.
-// Esta es una forma de crear interfaces de forma implicita
-type Figure interface {
-	Area() float64
-	Perimeter() float64
+/*
+	En los programas hasta ahora creados, las líneas aparecen de forma secuencial. Una línea no se ejecuta hasta que la anterior se complete, lo que es un comportamiento deseado en muchos casos.
+
+	Sin embargo también podemos toparnos con casos en los cuales una actividad no bloquea otra, al menos no si el tiempo que tomará en completarse es suficiente como para generar un bloqueo significativo. Algunas de estas acciones pueden ser tareas como una solicitud de lectura desde la red o la lectura de datos desde un archivo.
+
+	Go utiliza la concurrencia, que consiste en separar múltiples tareas de forma individual sin que estas bloqueen unas a las otras. Otra aproximación de otros lenguajes es el paralelismo.
+
+	La concurrencia es el poder ejecutar diferentes partes de un algoritmo, programa, solucion o tarea de forma independiente una de la otra, para que sin importar el orden en el que ejecutemos estas partes el resultado siempre sea igual.
+
+
+	Segun  Rob Pike creador de GO:
+	La concurrencia consiste en tratar muchas cosas a la vezen cambio el paralelismo consiste en hacer muchas cosas a la vez. No es lo mismo, pero está relacionado.
+
+	La concurrencia tiene que ver con la estructura, el paralelismo con la ejecución.
+
+	La meta de la concurrencia no es el paralelismo, si no es tener una buena estructura.
+
+	La concurrencia proporciona una forma de estructurar una solución para resolver un problema que puede (pero no necesariamente) ser paralelizable.
+
+	La concurrencia es una forma de estructurar un programa dividiéndolo en piezas que pueden ejecutarse de forma independiente. Este es el modelo de GO.
+
+	revisar: https://go.dev/talks/2012/waza.slide#28
+
+*/
+
+func say(text string, wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println(text)
 }
 
-type Circle struct {
-	radius float64
-}
-
-func (c Circle) Area() float64 {
-	return math.Pi * c.radius * c.radius
-}
-
-func (c Circle) Perimeter() float64 {
-	return math.Pi * 2 * c.radius
-}
-
-type Square struct {
-	side float64
-}
-
-func (s Square) Area() float64 {
-	return s.side * s.side
-}
-
-func (s Square) Perimeter() float64 {
-	return 4 * s.side
-}
-
-func Calcular(f Figure) {
-	fmt.Println("El área del ", reflect.TypeOf(f).Name(), "es:", f.Area())
-	fmt.Println("El perímetro del ", reflect.TypeOf(f).Name(), "es:", f.Perimeter())
-}
-
-func LessArea(s1, s2 Figure) Figure {
-	if s1.Area() < s2.Area() {
-		return s1
-	}
-	return s2
-}
+// la funcion main esta corriendo dentro de una go rutine, que una ves que termina de ejecutarse muere.
 
 func main() {
-	mySquare := Square{side: 2}
-	myCircle := Circle{radius: 4}
 
-	Calcular(mySquare)
-	Calcular(myCircle)
+	var wg sync.WaitGroup
+	fmt.Println("Hello")
+	wg.Add(1)
 
-	minArea := LessArea(myCircle, mySquare)
+	go say("World", &wg) // agregando go, crearas una gorutine el cual se ejecutará de forma concurrente
+	wg.Wait()
 
-	fmt.Println("The smallest area of the two figures is", reflect.TypeOf(minArea).Name())
+	wg.Add(1)
 
-	fmt.Println("\n*********** empty interface ****************")
-	// Una interfaz vacía puede contener valores de cualquier tipo. Las interfaces vacías son utilizadas por el código que maneja valores de tipo desconocido.
+	go func(text string, wg *sync.WaitGroup) { // funcion anonima
+		defer wg.Done()
+		fmt.Println(text)
+	}("Bye", &wg)
 
-	var value interface{}
-	value = "hola"
-
-	fmt.Println("Value: ", value)
-
-	value = 12
-
-	fmt.Println("Value: ", value)
-
-	fmt.Println("\n*********** Lista de interfaces ****************")
-
-	// Lista de interfaces
-
-	/*  Usualmente en muchos lenguajes de programacion mucho mas flexibles  a una misma lista puedes agregar diferente tipo de datos, por ejemplo: [1, "bo", true]. En GO no puedes hacer eso con los slices, porque tienes que indicar que tipo de dato vas a ingresar
-
-	Entonces, ¿como podemos simular eso en GO?
-
-	creamos un slice, lo llamaremos myInterface, y este va a instanciar  un slice de interfaces vacias. Como sabemos las interfaces vacias aceptan valores de cualquier tipo. En otras palabras creamos un slice donde cada elemento de este es un slice vacio.
-
-	*/
-
-	myInterface := []interface{}{"Hola", 1, 2.53, true}
-
-	fmt.Println(myInterface...) //con los tres punto imprimimos los elementos sin el array, parecido al spread de js
-
+	wg.Wait()
 }
+
+/*
+	func main() {
+		say("Hello")
+		go say("World")
+
+
+	}
+
+	cuando ejecutamos lo de arriba, veremos que World no se imprimirá, y esto es porque main se corre dentro una gorutina y una ves termine muere. Pues main termina a Hello porque la World se ejecuta de forma concurrente y no queda en el mismo hilo de ejecucion con main.
+
+	¿Como lo solucionamos?
+
+	Una accion basica, pero recomendable, es agregando un sleep.
+
+	func main() {
+		say("Hello")
+		go say("World")
+		time.Sleep(time.Second * 1)
+
+	}
+
+	Pero esta forma no es optima, porque estamos agregaando tiempo muerto
+
+	Hay una forma algo avanzado, usando wait group
+
+	Un WaitGroup espera a que una colección de goroutines termine su ejecución.
+	Para esto se una la WaitGroup.Add() ( wg.add(1) en el ejemplo de la clase).
+	El número entero indica el número de goroutines que debe esperar para finalizar la ejecución de la goroutine principal.
+
+	Cada vez que una goroutine termina su ejecución, llama el método Done(). Esto hace que el contador del WaitGroup se reduzca.
+	Cuando el contador llegue a zero la rutina principal continuará su ejecución.
+
+	vLa función wait() bloquea la rutina principal hasta que todas las demás rutinas del grupo hayan terminado.
+
+
+*/
